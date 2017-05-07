7298f455602e30d23ee6492e716b3c888c53b4a9
KirillGaisin <arcticnetherwind@gmail.com>
2017-05-07 14:20:48 +0300

    translated all printlines to english

--- Sito/src/com/kirill/web/SitoUniqueWords.java
+++ Sito/src/com/kirill/web/SitoUniqueWords.java
@@ -24,9 +24,9 @@ public class SitoUniqueWords {
 									("C:\\Users\\Zoddtheimmortal\\Desktop\\testfile1.txt")));
 			
 			Scanner sc = new Scanner(System.in);
-			System.out.println("Сортировать слова из файла в алфавитном порядке? Да/Нет");
+			System.out.println("Sort the words in alphabetical order? Y/N");
 			String alphabet = sc.nextLine();
-			System.out.println("Вывести число повторяющихся в файле слов? Да/Нет");
+			System.out.println("Show the number of duplicates? Y/N");
 			String duplicates = sc.nextLine().toLowerCase();
 			
 			sc.close();
@@ -37,15 +37,14 @@ public class SitoUniqueWords {
 				while ((strLine = br.readLine()) != null){
 					sortWords.addAll(Arrays.asList(strLine.split("\\s")));
 					Collections.sort(sortWords);
-					//System.out.printf("следующая строка - %s\n", strLine);	
 				}
 				
-				if(alphabet.equalsIgnoreCase("да"))
-					System.out.println("Отсортированные в алфавитном порядке слова: ");
+				if(alphabet.equalsIgnoreCase("y"))
+					System.out.println("Words sorted in alphabetical order: ");
 					for (String alphabetSorted : sortWords)
 						System.out.println(alphabetSorted);
 			
-				if(duplicates.equalsIgnoreCase("да")) {
+				if(duplicates.equalsIgnoreCase("y")) {
 					
 				Map<String, Integer> dupeMap = new HashMap<>();
 				for (String name : sortWords)
@@ -65,7 +64,7 @@ public class SitoUniqueWords {
 						iterator.remove();
 				}
 				
-				System.out.printf("Дубликаты: %s\n", dupes);
+				System.out.printf("Duplicates: %s\n", dupes);
 
 			}
 				
@@ -83,13 +82,12 @@ public class SitoUniqueWords {
 									(new FileOutputStream(uniqueWords))));
 				
 				for(String w : words)
-					//System.out.printf("пишем строку %s\n", w);
 					bw.println(w);
 				
 				bw.flush();
 				bw.close();
 				
-				System.out.println("Файл " + uniqueWords + " обновлен.");
+				System.out.println("File " + uniqueWords + " updates.");
 				
 			} catch (IOException ex) {
 				System.out.println(ex.getMessage());
