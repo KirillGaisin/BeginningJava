7075ce3fe6a89551757ebb1b4de7c1e253ce4c83
KirillGaisin <arcticnetherwind@gmail.com>
2017-05-11 11:42:52 +0300

    A simple JavaFX browser I made while learning at JavaSE8 level 2 course.

---
+++ FXMLKirillsBrowser/src/ru/specialist/webbrowser/Main.java
@@ -0,0 +1,32 @@
+package ru.specialist.webbrowser;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().
+					getResource("MainWin.fxml"));
+			
+			Scene scene = new Scene(root,1100,700);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
